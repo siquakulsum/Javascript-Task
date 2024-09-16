5dfdd54f10ba8ad99e08bd64ca0ce9d0340267f0
siquakulsum <siquakulsum@gmail.com>
2024-09-16 19:15:16 +0530

    changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateCountdown() {
     const now = new Date();
-    const birthday = new Date(now.getFullYear(), 8, 7); // September is month 9 (0-based index)
+    const birthday = new Date(now.getFullYear(), 8, 17); // September is month 9 (0-based index)
 
     if (now > birthday) {
         birthday.setFullYear(birthday.getFullYear() + 1);
@@ -19,9 +19,9 @@ function updateCountdown() {
 
     const messageElement = document.getElementById('message');
     const today = new Date();
-    if (today.getMonth() === 8 && today.getDate() === 7) {
-        messageElement.innerHTML = `<p>🎉 Happy Birthday, Zarmeen! 🎉</p>
-                                    <p>You light up every room you enter, and today, we celebrate the sparkle you bring into our lives.</p>`;
+    if (today.getMonth() === 8 && today.getDate() === 17) {
+        messageElement.innerHTML = `<p>🎉 Happy Birthday, Zaid! 🎉</p>
+                                    <p>Wishing you a day filled with love, laughter, and happiness! You've always been a wonderful part of our family, and we're so grateful for everything you do. May this year bring you even more success, joy, and cherished moments. Here's to celebrating you today and always. Enjoy your special day to the fullest!</p>`;
         messageElement.style.animation = 'sparkle 1.5s infinite';
     }
 }
